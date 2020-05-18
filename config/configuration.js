febca503e98d802758bdad9e996b025304e3d1c3
Bankole Ahmed <kidrolex19@gmail.com>
2020-05-18 03:40:21 +0100

    :zap: Added MongoDB dev link

--- config/configuration.js
+++ config/configuration.js
@@ -1,6 +1,6 @@
 module.exports = {
     mongoDbUrl: 'mongodb+srv://ahmed:adesewa19@cluster0-qq2gg.mongodb.net/geeky_codes?retryWrites=true&w=majority',
-    // mongoDbUrl: 'mongodb://localhost:27017/bankoleahmed',
+    mongoDbUrl_dev: 'mongodb://localhost:27017/bankoleahmed',
     PORT: process.env.PORT || 3000,
     globalVariables: (req, res, next) => {
         res.locals.success_message = req.flash('success-message');
