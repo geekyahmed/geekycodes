2e91d0d5980c52e9f3a47bfd0b7ca78f099f3e24
Bankole Ahmed <kidrolex19@gmail.com>
2020-05-27 02:43:19 +0100

    added urls to cache

--- public/sw.js
+++ public/sw.js
@@ -1,8 +1,10 @@
-var CACHE_NAME = 'version-2'; // bump this version when you make changes.
+var CACHE_NAME = 'version-3'; // bump this version when you make changes.
 // Put all your urls that you want to cache in this array
 var urlsToCache = [
     '/',
-    '/assets/logo-192.png'
+    '/images/avatarc.png',
+    '/login',
+    '/register'
 ];
 
 // Install the service worker and open the cache and add files mentioned in array to cache
